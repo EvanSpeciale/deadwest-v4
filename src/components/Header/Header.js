84409da095818c6aa9d9c48e3a1656d96fb5e241
EvanSpeciale <evspeciale@gmail.com>
2024-06-23 15:49:00 -0700

    starting storefront

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,34 @@
 import Link from 'next/link';
+import { useSnipcart } from 'use-snipcart';
 import { FaShoppingCart } from 'react-icons/fa';
+import { useRouter } from 'next/router';
 
 import Container from '@components/Container';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const { locale: activeLocale, locales, asPath } = useRouter();
+  const availableLocales = locales.filter(locale => locale !== activeLocale);
+  const { cart = {} } = useSnipcart();
+  const { subtotal = '0' } = cart;
   return (
     <header className={styles.header}>
       <Container className={styles.headerContainer}>
         <p className={styles.headerTitle}>
           <Link href="/">
-            <a>Space Jelly</a>
+            <a>Dead West</a>
           </Link>
         </p>
         <ul className={styles.headerLinks}>
           <li>
-            <Link href="#">
-              <a>Link</a>
+            <Link href="/categories/featured">
+              <a>Featured</a>
             </Link>
           </li>
           <li>
-            <Link href="#">
-              <a>Link</a>
+            <Link href="/categories/apparel">
+              <a>Apparel</a>
             </Link>
           </li>
           <li>
@@ -32,21 +38,28 @@ const Header = () => {
           </li>
         </ul>
         <p className={styles.headerCart}>
-          <button>
+          <button className='snipcart-checkout'>
             <FaShoppingCart />
             <span>
-              $0.00
+              ${subtotal}
             </span>
           </button>
         </p>
         <ul className={styles.headerLocales}>
           <li>
             <Link href="#">
-              <a>
-                ES
-              </a>
+              <a className='activeLocale'>{activeLocale}</a>
             </Link>
           </li>
+          {availableLocales.map(locale => {
+            return (
+              <li key={locale}>
+                <Link href={asPath} locale={locale}>
+                  <a className='localeOption'>{locale}</a>
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </Container>
     </header>
---
+++ src/pages/products/[productSlug].js
@@ -0,0 +1,139 @@
+import Head from 'next/head'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { buildImage } from '@lib/cloudinary';
+
+import Layout from '@components/Layout';
+import Header from '@components/Header';
+import Container from '@components/Container';
+import Button from '@components/Button';
+
+import styles from '@styles/Product.module.scss'
+
+export default function Product({ product }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{product.name}</title>
+        <meta name="description" content={`Dead West: ${product.name}`} />
+      </Head>
+
+      <Container>
+        <div className={styles.productWrapper}>
+          <div className={styles.productImage}>
+            <img src={buildImage(product.images[0].public_id).toURL()} />
+          </div>
+          <div className={styles.productContent}>
+            <h1>{product.name}</h1>
+            <div className={styles.productDescription} dangerouslySetInnerHTML={{ __html: product.description?.html }} />
+            <p className={styles.productPrice}>
+              ${product.price}
+            </p>
+            <p className={styles.productBuy}>
+              <Button className="snipcart-add-item"
+                data-item-id={product.id}
+                data-item-price={product.price}
+                data-item-description={product.description?.text}
+                data-item-image={product.images[0].url}
+                data-item-name={product.name}>
+                Add to Cart
+              </Button>
+            </p>
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params, locale }) {
+  const client = new ApolloClient({
+    uri: 'https://api-us-west-2.hygraph.com/v2/clxlea61c02nq06unyx15yc9m/master',
+    cache: new InMemoryCache()
+  })
+
+  const data = await client.query({
+    query: gql`
+      query PageProduct($slug: String, $locale: Locale!) {
+        product(where: {slug: $slug}) {
+          id
+          name
+          description {
+            html,
+            text
+          }
+          images
+          price
+          slug
+          localizations(locales: [$locale]) {
+            description {
+              html
+            }
+            locale
+          }
+        }
+      }
+    `,
+    variables: {
+      slug: params.productSlug,
+      locale: locale
+    }
+  })
+
+  let product = data.data.product;
+  if (product.localizations.length > 0) {
+    product = {
+      ...product,
+      ...product.localizations[0]
+    }
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+export async function getStaticPaths({ locales }) {
+  const client = new ApolloClient({
+    uri: 'https://api-us-west-2.hygraph.com/v2/clxlea61c02nq06unyx15yc9m/master',
+    cache: new InMemoryCache()
+  })
+
+  const data = await client.query({
+    query: gql`
+      query PageProducts {
+        products {
+          name
+          description {
+            html
+          }
+          images
+          price
+          slug
+        }
+      }
+    `
+  })
+  const paths = data.data.products.map(product => {
+    return {
+      params: {
+        productSlug: product.slug
+      }
+    }
+  })
+  return {
+    paths: [
+      ...paths,
+      ...paths.flatMap(path => {
+        return locales.map(locale => {
+          return {
+            ...path,
+            locale
+          }
+        })
+      })
+    ],
+    fallback: false
+  };
+}
