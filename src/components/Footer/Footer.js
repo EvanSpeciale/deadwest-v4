4e3c537ff8ffea99f3a7f14c83337cc1860614b1
EvanSpeciale <evspeciale@gmail.com>
2024-07-12 15:09:46 -0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 const Footer = ({ ...rest }) => {
   return (
     <footer className='round'{...rest}>
-      <div classname="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <span className="block text-sm text-gray-200 sm:text-center pb-4">
           &copy; <a href="deadwest.art">Dead West</a>, {new Date().getFullYear()}
         </span>
