8956367e695c769780ebe56a8cfc92bc3520d099
EvanSpeciale <evspeciale@gmail.com>
2024-07-25 14:38:36 -0700

    spacing bug

--- src/pages/products/[productSlug].js
+++ src/pages/products/[productSlug].js
@@ -29,7 +29,7 @@ export default function Product({ product }) {
               })}
             </div>
           </div>
-          <div className="mx-4 mt-0 sm:mt-4 bg-desert-green-light bg-opacity-80 rounded-lg">
+          <div className="mx-4 mt-4 sm:mt-0 bg-desert-green-light bg-opacity-80 rounded-lg">
             <div className='font-playwrite text-desert-green-dark text-3xl md:text-4xl lg:text-5xl mx-6 mt-8'>{product.name}</div>
             <div className="font-sans mx-6 mt-8 text-2xl"> {product.description?.text}</div>
             <div className='flex justify-between mx-6 my-8'>
